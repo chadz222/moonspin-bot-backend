4e87d3616f5669621a11c686aa1cb2fb80e3ddd9
chadz222 <Chadcooper2312@gmail.com>
2025-06-28 17:39:15 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,25 +1,30 @@
-const TelegramBot = require('node-telegram-bot-api');
+import TelegramBot from 'node-telegram-bot-api';
 
 const token = process.env.BOT_TOKEN;
-const bot = new TelegramBot(token, { polling: true });
 
-const webAppUrl = 'https://moonspin-app-chadz222-sales.vercel.app';
+let bot;
 
-bot.onText(/\/start/, (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, '🚀 Launch MoonSpin', {
-    reply_markup: {
-      inline_keyboard: [
-        [
-          {
-            text: '🎮 Play MoonSpin',
-            web_app: { url: webAppUrl }
-          }
+if (!bot) {
+  bot = new TelegramBot(token, { polling: true });
+
+  bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, '🚀 Launch MoonSpin', {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: '🎮 Play MoonSpin',
+              web_app: {
+                url: 'https://moonspin-app-chadz222-sales.vercel.app'
+              }
+            }
+          ]
         ]
-      ]
-    }
+      }
+    });
   });
-});
+}
 
 export default function handler(req, res) {
   res.status(200).json({ ok: true });
